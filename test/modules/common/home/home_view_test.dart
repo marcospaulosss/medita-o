de70e94bfd35ed6addb50035b815d0bf53d6caf1
marcos paulo <marcospaulo.s.s@hotmail.com>
2023-08-23 20:48:55 -0300

    ajusta teste da home quebrado

--- test/modules/common/home/home_view_test.dart
+++ test/modules/common/home/home_view_test.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:cinco_minutos_meditacao/core/di/helpers.dart';
 import 'package:cinco_minutos_meditacao/modules/common/screens/home/home_presenter.dart';
 import 'package:cinco_minutos_meditacao/modules/common/screens/home/home_view.dart';
 import 'package:cinco_minutos_meditacao/modules/common/shared/strings/localization/common_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
@@ -11,16 +14,18 @@ import 'package:mockito/mockito.dart';
 import 'home_view_test.mocks.dart';
 
 @GenerateMocks([HomePresenter])
-void main() {
+main() async {
+  dotenv.testLoad(fileInput: File('.env').readAsStringSync());
+
   group("Home", () {
     late MockHomePresenter presenter;
     late HomeView homeView;
 
     setUp(() {
       presenter = MockHomePresenter();
-      homeView = const HomeView();
-
       registerFactory<HomePresenter>(() => presenter);
+
+      homeView = const HomeView();
     });
 
     testWidgets("Should show home screen", (tester) async {
