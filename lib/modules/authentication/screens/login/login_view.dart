fc845e3675fca0ef62bd5edcce9f8c0949a94f7e
marcos paulo <marcospaulo.s.s@hotmail.com>
2023-09-01 00:06:40 -0300

    Layout Login
    - Adiciona a imagem do icone do google ao projeto
    - Cria o service de autenticação com o firebase
    - Cria layout da tela de login utilizando os componente de backgroud gradiente e icon label button
    - Cria componente de background default com gradiente
    - Cria componente icon label button para o botão de login via google
    - Cria classe de cores para reutilizar no projeto

--- lib/modules/authentication/screens/login/login_view.dart
+++ lib/modules/authentication/screens/login/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/annotations.dart';
+import 'package:cinco_minutos_meditacao/shared/components/background_default.dart';
+import 'package:cinco_minutos_meditacao/shared/components/icon_label_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 @RoutePage()
 class LoginView extends StatelessWidget {
@@ -7,8 +10,27 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login'),
+    return Scaffold(
+      body: BackgroundDefault(
+        child: IconLabelButton(
+          onTap: () {},
+          width: 350,
+          height: 55,
+          image: SvgPicture.asset(
+            'assets/icons/google_logo.svg',
+            height: 30,
+          ),
+          label: const Text(
+            'Sua conta Google',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff2B448C),
+              fontFamily: 'Apertura',
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/shared/Theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+/// Classe que contém as cores do aplicativo.
+abstract class AppColors {
+  static const backgroundBlue = Color(0xff70D7F5);
+  static const white = Color(0xffffffff);
+}
