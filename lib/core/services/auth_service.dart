2d841a2759145e3471235d0c39ac81b6a7f47e02
marcos paulo <marcospaulo.s.s@hotmail.com>
2023-09-01 00:06:40 -0300

    Layout Login
    - Adiciona a imagem do icone do google ao projeto
    - Cria o service de autenticação com o firebase
    - Cria layout da tela de login utilizando os componente de backgroud gradiente e icon label button
    - Cria componente de background default com gradiente
    - Cria componente icon label button para o botão de login via google
    - Cria classe de cores para reutilizar no projeto

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -1,5 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class AuthService {
-  bool get _authenticated => true;
+  /// Credenciais para o firebase
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  late final FirebaseApp app;
+  late final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool _authenticated = false;
 
   bool get isAuthenticated => _authenticated;
+
+  Future<(GoogleSignInAccount?, AuthCredential?, Object?)> loginGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
+
+      /// Credenciais para o firebase
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      print("Google Nome: ${googleUser.displayName}");
+      print("Google Email: ${googleUser.email}");
+      print("Google Foto: ${googleUser.photoUrl}");
+      print("Google id: ${googleUser.id}");
+
+      UserCredential result = await _auth.signInWithCredential(credential);
+      final User fireUser = result.user!;
+      print("firebase Nome: ${fireUser.displayName}");
+      print("firebase Email: ${fireUser.email}");
+      print("firebase Foto: ${fireUser.photoURL}");
+      print("firebase metadata: ${fireUser.metadata}");
+      print("firebase phone: ${fireUser.phoneNumber}");
+      print("firebase id: ${fireUser.uid}");
+
+      _authenticated = true;
+
+      return (googleUser, credential, null);
+    } catch (e) {
+      return (null, null, Future.error(e));
+    }
+  }
+
+  Future<Object?> logout() async {
+    try {
+      await _googleSignIn.signOut();
+      await _auth.signOut();
+      _authenticated = false;
+    } catch (e) {
+      return e;
+    }
+
+    return null;
+  }
 }
