5e1a7b89abcb26f9dc7a41334927d12a1dfc51d9
marcos paulo <marcospaulo.s.s@hotmail.com>
2023-09-05 00:13:29 -0300

    Login
    - Adiciona strings ao modulo de authentication
    - Cria testes unitariospara a tela de login

---
+++ lib/modules/authentication/shared/strings/localization/authentication_strings_pt.dart
@@ -0,0 +1,9 @@
+import 'authentication_strings.dart';
+
+/// The translations for Portuguese (`pt`).
+class AuthenticationStringsPt extends AuthenticationStrings {
+  AuthenticationStringsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get yourGoogleAccount => 'Sua conta Google';
+}
