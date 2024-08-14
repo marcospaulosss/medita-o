8e0f32c1d6702d19d7e001055784801ce48e46ed
marcos paulo sousa santos <marcos.santos@stuo.app>
2024-08-13 22:08:57 -0300

    Faz layout da tela de programa 5 minutos

---
+++ lib/shared/Theme/app_tracks.dart
@@ -0,0 +1,7 @@
+import 'package:audioplayers/audioplayers.dart';
+
+/// Classe que contém as cores do aplicativo.
+abstract class AppTracks {
+  /// blue variations
+  static AssetSource trackFive = AssetSource('audios/track_5.m4a');
+}
