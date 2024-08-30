4e54904ab203260eba20a995424b4470e55aef49
marcospaulosss <marcospaulo.s.s@hotmail.com>
2024-08-30 12:21:42 -0300

    - abertura de teclado no login - abertura de teclado no cadastro - gravação de meditação ao finalizar - alteração do link do video do youtube

--- lib/modules/meditate/screens/in_your_time/in_your_time_view.dart
+++ lib/modules/meditate/screens/in_your_time/in_your_time_view.dart
@@ -37,7 +37,14 @@ class InYourTimeViewState extends State<InYourTimeView>
   /// Mensagem de erro
   late String messageError = "";
 
+  /// Player de áudio
   late AudioPlayer player;
+
+  /// Se a reprodução foi concluída
+  bool hasCompleted = false;
+
+  /// Tempo da meditação
+  int time = 5;
 
   @override
   void initState() {
@@ -50,7 +57,7 @@ class InYourTimeViewState extends State<InYourTimeView>
     player = AudioPlayer();
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await getTrack(5);
+      await getTrack(time);
     });
 
     super.initState();
@@ -96,14 +103,7 @@ class InYourTimeViewState extends State<InYourTimeView>
         children: [
           Player(
             player: player,
-            onStop: () async {
-              print("funcionando porra");
-              if (player.processingState == ProcessingState.idle) {
-                // Recarregar o áudio se o player estiver ocioso
-                await player
-                    .setAudioSource(AudioSource.asset(AppTracks.trackFive));
-              }
-            },
+            onStop: submitMeditateComplete,
           ),
           const SizedBox(height: 20),
           Text(
@@ -142,24 +142,31 @@ class InYourTimeViewState extends State<InYourTimeView>
     switch (minutes) {
       case 5:
         track = AppTracks.trackFive;
+        time = 5;
         break;
       case 10:
         track = AppTracks.trackTen;
+        time = 10;
         break;
       case 15:
         track = AppTracks.trackFifteen;
+        time = 15;
         break;
       case 20:
         track = AppTracks.trackTwenty;
+        time = 20;
         break;
       case 25:
         track = AppTracks.trackTwentyFive;
+        time = 25;
         break;
       case 30:
         track = AppTracks.trackThirty;
+        time = 30;
         break;
       default:
         track = AppTracks.trackFive;
+        time = 5;
     }
 
     await player.setAudioSource(AudioSource.asset(
@@ -167,6 +174,19 @@ class InYourTimeViewState extends State<InYourTimeView>
     ));
 
     setState(() {});
+  }
+
+  /// Submete a meditação como concluída
+  submitMeditateComplete() async {
+    if (player.processingState == ProcessingState.idle) {
+      // Recarregar o áudio se o player estiver ocioso
+      await player.setAudioSource(AudioSource.asset(AppTracks.trackFive));
+    }
+
+    if (!hasCompleted) {
+      presenter.submitMeditateCompleted(time);
+      hasCompleted = true;
+    }
   }
 
   /// Mostra o estado de carregamento
@@ -186,5 +206,10 @@ class InYourTimeViewState extends State<InYourTimeView>
   void showError(String message) {
     messageError = message;
     stateController.showErrorState();
+  }
+
+  @override
+  void meditationCompleted() {
+    hasCompleted = false;
   }
 }
