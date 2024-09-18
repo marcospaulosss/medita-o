d4ebf0019cb8384758ea77d515859e121747f54d
marcospaulosss <marcospaulo.s.s@hotmail.com>
2024-09-17 23:09:49 -0300

    cria arquitetura da meditaçao guiada

--- lib/modules/meditate/di/setup.dart
+++ lib/modules/meditate/di/setup.dart
@@ -1,4 +1,5 @@
 import 'package:cinco_minutos_meditacao/modules/meditate/di/five_minutes.dart';
+import 'package:cinco_minutos_meditacao/modules/meditate/di/guided_meditation.dart';
 import 'package:cinco_minutos_meditacao/modules/meditate/di/in_your_time.dart';
 import 'package:cinco_minutos_meditacao/modules/meditate/di/meditate_info.dart';
 
@@ -6,4 +7,5 @@ void setupInjectors() {
   MeditateInfoInjector.setup();
   FiveMinutesInjector.setup();
   InYourTimeInjector.setup();
+  GuidedMeditationInjector.setup();
 }
