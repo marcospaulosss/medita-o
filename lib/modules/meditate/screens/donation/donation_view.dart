308ebbcd456093b594e43e93eeccbb6534386ab3
marcospaulosss <marcospaulo.s.s@hotmail.com>
2024-09-23 22:45:52 -0300

    work flow prod

--- lib/modules/meditate/screens/donation/donation_view.dart
+++ lib/modules/meditate/screens/donation/donation_view.dart
@@ -1,29 +1,28 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:cinco_minutos_meditacao/core/di/helpers.dart';
-import 'package:cinco_minutos_meditacao/modules/meditate/screens/guided_meditation/guided_meditation_contract.dart';
-import 'package:cinco_minutos_meditacao/modules/meditate/screens/guided_meditation/guided_meditation_presenter.dart';
-import 'package:cinco_minutos_meditacao/modules/meditate/shared/strings/localization/meditate_strings.dart';
+import 'package:cinco_minutos_meditacao/modules/meditate/screens/donation/donation_contract.dart';
+import 'package:cinco_minutos_meditacao/modules/meditate/screens/donation/donation_presenter.dart';
 import 'package:cinco_minutos_meditacao/shared/Theme/app_colors.dart';
 import 'package:cinco_minutos_meditacao/shared/components/app_background.dart';
 import 'package:cinco_minutos_meditacao/shared/helpers/view_binding.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class GuidedMeditationView extends StatefulWidget {
+class DonationView extends StatefulWidget {
   /// Construtor
-  const GuidedMeditationView({
+  const DonationView({
     super.key,
   });
 
   @override
-  State<GuidedMeditationView> createState() => GuidedMeditationViewState();
+  State<DonationView> createState() => DonationViewState();
 }
 
 @visibleForTesting
-class GuidedMeditationViewState extends State<GuidedMeditationView>
-    implements GuidedMeditationViewContract {
+class DonationViewState extends State<DonationView>
+    implements DonationViewContract {
   /// Presenter
-  Presenter presenter = resolve<GuidedMeditationPresenter>();
+  Presenter presenter = resolve<DonationPresenter>();
 
   /// Mensagem de erro
   late String messageError = "";
@@ -82,31 +81,81 @@ class GuidedMeditationViewState extends State<GuidedMeditationView>
               ),
               child: Column(
                 children: [
-                  Text(
-                    MeditateStrings.of(context).aboutGuidedMeditation,
-                    style: const TextStyle(
-                      fontSize: 52,
-                      fontFamily: 'Blanch',
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.germanderSpeedwell,
+                  Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(16)),
+                        child: Image.network(
+                          'https://via.placeholder.com/600x200',
+                          height: 200,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Positioned(
+                        top: 80,
+                        left: MediaQuery.of(context).size.width / 2 - 25,
+                        child: IconButton(
+                          icon: Icon(Icons.play_circle_fill,
+                              color: Colors.blue, size: 50),
+                          onPressed: () {
+                            // Ação ao clicar no botão de play
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'SUA DOAÇÃO AJUDA A PAZ SER CADA VEZ MAIS MUNDIAL!',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Todas as doações recebidas ajudam o Instituto Mãos Sem Fronteiras a capacitarem mais voluntários e levarem a paz para cada vez mais pessoas.',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 16),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Ação ao clicar no botão de doar
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: Text(
+                              'DOAR',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 18),
-                  Text(
-                    MeditateStrings.of(context).titleGuidedMeditation,
-                    textAlign: TextAlign.center,
-                    style: buildTextStyleDefault(),
-                  ),
-                  const SizedBox(height: 29),
-                  Image.asset(
-                    'assets/images/fundation.png',
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(height: 28),
-                  Text(
-                    MeditateStrings.of(context).descriptionGuidedMeditation,
-                    textAlign: TextAlign.center,
-                    style: buildTextStyleDefault(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.justify,
+                    ),
                   ),
                 ],
               ),
